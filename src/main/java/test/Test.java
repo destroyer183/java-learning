dedf3be688dbda0cc12fa51b140448caf6eadb15
destroyer183 <jzante99@gmail.com>
2023-09-16 19:54:51 -0400

    adding stuff

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		int x = 5;
+		
+		float y = 11;
+		
+		boolean z = true;
+		
+		for (int i=0; i <= 10; i++) {
+			
+			System.out.println("hello world %d" i);
+		}
+	}
+
+}
